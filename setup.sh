bd0d292600f340e8518be0b02bbdf7d1d53f56aa
Milkthedev <pranavlal896@gmail.com>
2024-05-11 21:08:01 +0530

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -2,18 +2,24 @@
 
 # Update package lists
 sudo apt update
+apt update
 
 # Install Python 3 and pip
 sudo apt install -y python3 python3-pip
+apt install -y python3 python3-pip
 
 # Install requests library
+sudo pip3 install requests
 pip3 install requests
 
 # Install Java Runtime Environment (JRE)
 sudo apt install -y default-jre
+apt install -y default-jre
 
 # Install git
 sudo apt install -y git
+apt install -y git
 
 # Clone the repository
+sudo git clone https://github.com/SpigotRCE/StormLinker
 git clone https://github.com/SpigotRCE/StormLinker
